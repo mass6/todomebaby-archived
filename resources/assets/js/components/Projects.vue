37cd45f71cc4f6fb312d49acb8d8de4933bf2d79
Sam <samcharmy@gmail.com>
2016-09-15 22:34:54 +0200

    Hide mobile sidebar after clicking "New Project" link

--- resources/assets/js/components/Projects.vue
+++ resources/assets/js/components/Projects.vue
@@ -4,7 +4,7 @@
             <i class="icon-stack"></i> <span>Projects</span>
         </a>
         <ul>
-            <a v-link="{ name: 'projects.create' }" id="add-project" class="add-project pull-right" alt="Add Project"><i class="icon-plus-circle2"></i></a>
+            <a v-link="{ name: 'projects.create' }" id="add-project" class="add-project pull-right" alt="Add Project" @click="hideMobileSidebar"><i class="icon-plus-circle2"></i></a>
             <li v-for="project in sharedState.projects">
                 <a v-link="{ name: 'projects.show', params: { id: project.id }, replace: true, exact: true }" class="project-link" v-link-active @click.stop="taskListSelected">
                     {{ project.name }}<span id="project-{{project.id}}-task-count" class="badge badge-primary bg-blue-tdm border-blue-tdm">{{ project.taskCount }}</span>
@@ -28,6 +28,9 @@
         methods: {
             taskListSelected: function () {
                 this.$dispatch('taskListWasSelected');
+            },
+            hideMobileSidebar: function() {
+                $('body').toggleClass('sidebar-mobile-main').removeClass('sidebar-mobile-secondary sidebar-mobile-opposite sidebar-mobile-detached');
             }
         }
     }
