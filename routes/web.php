aac81f8908876f3315d1ac50affd71e948477020
Sam <samcharmy@gmail.com>
2016-09-15 20:37:23 +0200

    Move Auth routes into local file and disable registration

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,26 @@
 Route::get('test', function () {
     return view('welcome');
 });
-
-
-Auth::routes();
-Route::get('/home', 'HomeController@index');
-
+//Route::get('/home', 'HomeController@index');
 Route::get('/', function () {
     return redirect('/web');
 });
+
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout');
+
+// Registration Routes...
+//$this->get('register', 'Auth\RegisterController@showRegistrationForm');
+//$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
 Route::get('/web/{vue?}', ['middleware' => 'auth', function () {
     return view('webapp');
 }])->where('vue', '[\/\w\.-]*');
