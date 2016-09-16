6786c40386cab6a2cd36a7b42333f03759382752
Sam <samcharmy@gmail.com>
2016-09-16 23:12:53 +0200

    Edit database config to work with live server

--- config/database.php
+++ config/database.php
@@ -54,6 +54,20 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
+        'mysql-local' => [
+            'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost') . ('homestead' == gethostname() ? null : ':33060'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
