f8a9cf9e1846c3221a89a698d0c509ce4ffce56b
Sam <samcharmy@gmail.com>
2016-09-22 11:26:26 +0200

    Delete unnecessary use statement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use App\Task;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
