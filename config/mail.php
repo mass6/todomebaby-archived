60bee6f72f6ade5af5694defda4688a932c33da5
Sam <samcharmy@gmail.com>
2016-09-23 11:51:16 +0200

    Configure mail

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'hello@todomebaby.com',
+        'name' => 'TodoMeBaby',
     ],
 
     /*
