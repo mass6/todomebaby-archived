c870c580641761af043ef245ada8e8be75b1f747
Sam <samcharmy@gmail.com>
2016-09-24 23:07:07 +0200

    Update TaskModel unit tests

--- tests/unit/TaskModelTest.php
+++ tests/unit/TaskModelTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Project;
 use App\Task;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -32,7 +34,6 @@ class TaskModelTest extends TestCase
      * Service scopes queries to specified user
      *
      * @test
-     * @group failing
      */
     public function it_scopes_queries_to_the_specified_user()
     {
@@ -45,6 +46,123 @@ class TaskModelTest extends TestCase
         $this->assertCount(5, $scopedUsersTasks);
     }
 
+    /**
+     * Service scopes queries to open tasks
+     *
+     * @test
+     */
+    public function it_applies_the_open_scope()
+    {
+        // given I have an open and a closed task
+        $tasks = $this->generateUserTasks(2);
+        $tasks[0]->complete = true;
+        $tasks[0]->save();
+
+        // when I run a query with the openScope applied
+        $openTasks = Task::open()->get();
+
+        // then it should only return tasks which are open
+        $this->assertCount(1, $openTasks);
+        $this->assertEquals($tasks[1]->title, $openTasks->first()->title);
+    }
+
+    /**
+     * Scope return task list with relations and sorting applied, with closed
+     * tasks if includeCompleted parameter is true
+     *
+     * @test
+     */
+    public function it_applies_the_task_list_scope()
+    {
+        // Sorting order: Next/true, due_date/asc, priority/desc, created/asc
+        // given I have 6 tasks:
+        // 0: next/true, due_date/null, priority/low - (Expected position: 2)
+        // 1: next/true, due_date/null, priority/high - (Expected position: 1)
+        // 2: next/true, due_date/today, priority/low - (Expected position: 0)
+        // 3: next/false, due_date/today, priority/low - (Expected position: 4)
+        // 4: next/false, due_date/today, priority/low (created_after #4) - (Expected position: 5)
+        // 5: next/false, due_date/today, priority/high - (Expected position: 3)
+        // 6: next/false, due_date/tomorrow, priority/low - (Expected position: 6)
+
+        $today = Carbon::today()->toDayDateTimeString();
+        $tasks = $this->generateUserTasks(7, null,[
+            'priority' => 'low',
+            'next' => false,
+            'due_date' => null,
+            'created_at' => Carbon::now()->subDays(5)
+        ]);
+        $tasks[0]->update(['next'=>true]);
+        $tasks[1]->update(['next'=>true, 'priority'=>'high']);
+        $tasks[2]->update(['next'=>true, 'due_date'=>$today]);
+        $tasks[3]->update(['due_date'=>$today]);
+        $tasks[4]->created_at = Carbon::now()->subDays(2);
+        $tasks[4]->update(['due_date'=>$today]);
+        $tasks[5]->update(['due_date'=>$today, 'priority'=>'high']);
+        $tasks[6]->update(['due_date'=>Carbon::tomorrow()->toDayDateTimeString()]);
+
+        $taskList = Task::taskList()->get();
+
+        // verify sort order
+        $this->assertEquals($tasks[2]->id, $taskList[0]->id);
+        $this->assertEquals($tasks[1]->id, $taskList[1]->id);
+        $this->assertEquals($tasks[0]->id, $taskList[2]->id);
+        $this->assertEquals($tasks[5]->id, $taskList[3]->id);
+        $this->assertEquals($tasks[3]->id, $taskList[4]->id);
+        $this->assertEquals($tasks[4]->id, $taskList[5]->id);
+        $this->assertEquals($tasks[6]->id, $taskList[6]->id);
+
+        // verify relations are present
+        $this->assertArrayHasKey('project', $taskList->first()->toArray());
+        $this->assertArrayHasKey('tags', $taskList->first()->toArray());
+
+    }
+
+
+    /**
+     * It toggles the next field flag
+     *
+     * @test
+     */
+    public function it_toggles_the_next_field()
+    {
+        $task = $this->generateUserTasks(1, null, ['next' => false]);
+        $task->toggleNextFlag();
+        $this->assertTrue($task->next);
+        $task->toggleNextFlag();
+        $this->assertFalse($task->next);
+    }
+
+    /**
+     * It toggles the completion status
+     *
+     * @test
+     */
+    public function it_toggles_the_completion_status()
+    {
+        $task = $this->generateUserTasks(1);
+
+        $task->toggleComplete();
+        $this->assertTrue($task->complete);
+
+        $task->toggleComplete();
+        $this->assertFalse($task->complete);
+    }
+
+
+    /**
+     * It associates a task to a project
+     *
+     * @test
+     * @group current
+     */
+    public function it_associates_task_to_project()
+    {
+        $project = Project::create(['name'=>'Project One', 'user_id'=>$this->user->id]);
+        $task = $this->generateUserTasks(1);
+
+        $task->associateToProject($project->id);
+        $this->assertEquals($project->id, $task->project->id);
+    }
 
     /**
      * Created_at field is set by ApplicationServiceProvider event method
