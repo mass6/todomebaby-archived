9e4995b7b7657054496cae813fd65b8827fe1f7e
Sam <samcharmy@gmail.com>
2016-09-26 10:29:22 +0200

    Update ProjectModel unit tests

--- tests/unit/ProjectModelTest.php
+++ tests/unit/ProjectModelTest.php
@@ -3,6 +3,7 @@
 use App\Project;
 use App\Task;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ProjectModelTest extends TestCase
@@ -48,19 +49,57 @@ class ProjectModelTest extends TestCase
     }
 
 
-    public function it_fetches_all_related_tasks_which_are_open()
+    /**
+     * It fetches related task list with relations and sorting applied, with closed
+     * tasks if includeCompleted parameter is true
+     *
+     * @test
+     */
+    public function it_applies_the_task_list_scope()
     {
-        $user = factory(User::class)->create([]);
-        $this->actingAs($user);
-        $project = $user->projects()->create(factory(Project::class)->make([])->toArray());
-        $tasks = factory(Task::class, 3)->create([
-            'user_id' => $user->id,
-            'project_id' => $project->id
-        ]);
-        $tasks->last()->toggleComplete();
-        //var_dump($tasks->toArray());
+        // Sorting order: Next/true, due_date/asc, priority/desc, created/asc
+        // Given I have a project with 7 tasks:
+        // 0: next/true, due_date/null, priority/low - (Expected position: 2)
+        // 1: next/true, due_date/null, priority/high - (Expected position: 1)
+        // 2: next/true, due_date/today, priority/low - (Expected position: 0)
+        // 3: next/false, due_date/today, priority/low - (Expected position: 4)
+        // 4: next/false, due_date/today, priority/low (created_after #4) - (Expected position: 5)
+        // 5: next/false, due_date/today, priority/high - (Expected position: 3)
+        // 6: next/false, due_date/tomorrow, priority/low - (Expected position: 6)
 
-        $this->assertEquals(2, $project->openTasks()->count());
+        $today = Carbon::today()->toDayDateTimeString();
+        $project = $this->generateProjects(1);
+        $tasks = $this->generateUserTasks(7, null,[
+            'project_id' => $project->id,
+            'priority' => 'low',
+            'next' => false,
+            'due_date' => null,
+            'created_at' => Carbon::now()->subDays(5)
+        ]);
+        $tasks[0]->update(['next'=>true]);
+        $tasks[1]->update(['next'=>true, 'priority'=>'high']);
+        $tasks[2]->update(['next'=>true, 'due_date'=>$today]);
+        $tasks[3]->update(['due_date'=>$today]);
+        $tasks[4]->created_at = Carbon::now()->subDays(2);
+        $tasks[4]->update(['due_date'=>$today]);
+        $tasks[5]->update(['due_date'=>$today, 'priority'=>'high']);
+        $tasks[6]->update(['due_date'=>Carbon::tomorrow()->toDayDateTimeString()]);
+
+        $taskList = $project->taskList();
+
+        // verify sort order
+        $this->assertEquals($tasks[2]->id, $taskList[0]->id);
+        $this->assertEquals($tasks[1]->id, $taskList[1]->id);
+        $this->assertEquals($tasks[0]->id, $taskList[2]->id);
+        $this->assertEquals($tasks[5]->id, $taskList[3]->id);
+        $this->assertEquals($tasks[3]->id, $taskList[4]->id);
+        $this->assertEquals($tasks[4]->id, $taskList[5]->id);
+        $this->assertEquals($tasks[6]->id, $taskList[6]->id);
+
+        // verify relations are present
+        $this->assertArrayHasKey('project', $taskList->first()->toArray());
+        $this->assertArrayHasKey('tags', $taskList->first()->toArray());
+
     }
 
 
@@ -72,6 +111,14 @@ class ProjectModelTest extends TestCase
         $overrides['user_id'] = $user ? $user->id : $this->user->id;
 
         return factory(Project::class, $amount)->create($overrides);
+    }
+
+
+    protected function generateUserTasks($amount = 5, User $user = null, $overrides = [])
+    {
+        $overrides['user_id'] = $user ? $user->id : $this->user->id;
+
+        return factory(Task::class, $amount)->create($overrides);
     }
 
 
