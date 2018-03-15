a3c1419f219f75d39c0113ae4b8f4f255de0b5eb
Sam <samcharmy@gmail.com>
2018-03-15 21:55:32 +0100

    Add User seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(User::class)->create([
+            'name' => 'Joe Moe',
+            'email' => 'joe@example.com',
+        ]);
     }
 }
